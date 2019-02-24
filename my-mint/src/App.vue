b8c25715bf7fe6cf76c6b2096b29ca62fb3d2d8e
xiaqingpeng <626143872@qq.com>
2019-02-24 16:09:02 +0800

    Action-Sheet

--- my-mint/src/App.vue
+++ my-mint/src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  
-
+  <div>
+    <actionSheet></actionSheet>
+  </div>
 </template>
 
 <script>
-
+import ActionSheet from "./components/action-sheet";
 export default {
- 
+  components: {
+    ActionSheet
+  }
 };
 </script>
