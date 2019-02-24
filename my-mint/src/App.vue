c2fa6f7aa17a9be16275b0d2cf1df9bf8fcf5dd4
xiaqingpeng <626143872@qq.com>
2019-02-24 15:37:59 +0800

    重新新建项项目

--- my-mint/src/App.vue
+++ my-mint/src/App.vue
@@ -1,14 +1,11 @@
 <template>
-    <mintheader></mintheader>
-
+  
 
 </template>
 
 <script>
-import mintheader from "@/components/Mint-Header.vue";
+
 export default {
-  components:{
-    mintheader
-  }
+ 
 };
 </script>
